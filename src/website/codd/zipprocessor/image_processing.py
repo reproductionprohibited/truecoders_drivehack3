a0a4bf997d1e5fa14d074554478efd3f210510ff
Stan Tomulets <stantom16@gmail.com>
2024-08-23 15:26:05 +0300

    Some touches

--- src/website/codd/zipprocessor/image_processing.py
+++ src/website/codd/zipprocessor/image_processing.py
@@ -52,7 +52,7 @@ class Preprocesser:
     def preprocess(self, img):
         norm_img = np.zeros((img.shape[0], img.shape[1]))
         img = cv2.normalize(img, norm_img, 0, 255, cv2.NORM_MINMAX)
-        img = img[:img.shape[0] - 20,]
+        img = img[20:img.shape[0] - 20,]
         img = cv2.fastNlMeansDenoisingColored(img, None, 10, 10, 7, 15)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         return img
